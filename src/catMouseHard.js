c257199e52e9fa1fa5af7aaf39e00bcf417bbbba
csernyi <csernyi@hu.ibm.com>
2023-04-14 01:55:12 +0200

    UAT 2.1 passed

--- src/catMouseHard.js
+++ src/catMouseHard.js
@@ -2,6 +2,7 @@ function catMouseHard(x, j) {
   if (!x.includes('D') || !x.includes('C') || !x.includes('m')){
     return 'boring without all three';
   }
+  return 'Caught!';
 }
 
 module.exports = catMouseHard;
